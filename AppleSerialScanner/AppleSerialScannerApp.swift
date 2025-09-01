f43b572db9f0dfef7b7fc4356a2b973160057017
amanmogal <amanmogal123@gmail.com>
2025-09-02 02:04:04 +0530

    Initial Commit

---
+++ AppleSerialScanner/AppleSerialScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleSerialScannerApp.swift
+//  AppleSerialScanner
+//
+//  Created by APPLE on 02/09/25.
+//
+
+import SwiftUI
+
+@main
+struct AppleSerialScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppleSerialScannerTests/AppleSerialScannerTests.swift
@@ -0,0 +1,16 @@
+//
+//  AppleSerialScannerTests.swift
+//  AppleSerialScannerTests
+//
+//  Created by APPLE on 02/09/25.
+//
+
+import Testing
+
+struct AppleSerialScannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
